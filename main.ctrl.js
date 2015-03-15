604cd2d437812103abf197ff1bd1508016b9e980
Kemal <ddeveloperr@gmail.com>
2015-03-15 10:30:43 +0100

    Add filters with ngRepeat and Form validation

--- main.ctrl.js
+++ main.ctrl.js
@@ -29,32 +29,31 @@ angular.module('app').controller("MainController", function(){
             favorite: false
         }
     ];
-
     vm.orders = [
-    {
-        id: 1,
-        title: 'Year Ascending',
-        key: 'year',
-        reverse: false
-    },
-    {
-        id: 2,
-        title: 'Year Descending',
-        key: 'year',
-        reverse: true
-    },
-    {
-        id: 3,
-        title: 'Title Ascending',
-        key: 'title',
-        reverse: false
-    },
-    {
-        id: 4,
-        title: 'Title Descending',
-        key: 'title',
-        reverse: true
-    }
-];
-vm.order = vm.orders[0];
+        {
+            id: 1,
+            title: 'Year Ascending',
+            key: 'year',
+            reverse: false
+        },
+        {
+            id: 2,
+            title: 'Year Descending',
+            key: 'year',
+            reverse: true
+        },
+        {
+            id: 3,
+            title: 'Title Ascending',
+            key: 'title',
+            reverse: false
+        },
+        {
+            id: 4,
+            title: 'Title Descending',
+            key: 'title',
+            reverse: true
+        }
+    ];
+    vm.order = vm.orders[0];
 });
